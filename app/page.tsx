9ab161d9517ed9cc17eaa5e1c856e9865748d30f
Davidson Rogerio de Aguiar <davidson.aguiar@compasso.com.br>
2025-04-28 21:34:57 -0300

    modificado nomes dos botoes

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,20 @@
 import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center gap-1 w-screen h-screen">
-      <div className="w-fit max-w-md mx-auto flex flex-col justify-center items-center gap-4 ">
+    <div className="flex flex-col justify-center items-cente w-screen h-screen">
+      <div className="w-full max-w-md mx-auto flex flex-col justify-center items-center gap-2">
 
-        <h2 className="mb-6 text-foreground text-3xl font-bold">Bem-vindo ao Hacka Pizza</h2>
+        <h2 className="w-full mb-6 text-zinc-700 text-3xl font-bold">Bem-vindo ao <br/><span className="text-sky-600 text-6xl">Hacka Pizza</span></h2>
 
         <Button asChild size="lg" className="w-full">
-          <a href="auth/login">
-            Ir para login
-          </a>
+          <a href="auth/login">Login</a>
         </Button>
 
-        <Button variant="ghost" asChild size="lg" className="w-full">
-          <a href="auth/register">
-            Ir para registro
-          </a>
+        <Button asChild variant="ghost" size="lg" className="w-full">
+          <a href="auth/register">Registre-se</a>
         </Button>
       </div>
     </div>
